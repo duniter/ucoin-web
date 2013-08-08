f886f9e021ec598da96cc33b9356e32ca59a5e1d
cgeek <cem.moreau@gmail.com>
2013-08-08 15:04:49 +0200

    Static web site working

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    currency: 'beta_brousoufs',
+    membersCount: 0,
+    amendmentsCount: 0,
+    transactionsCount: 0
+  });
 };
