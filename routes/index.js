1e86b23aceedd60cea5d482955cdf1508fa4fb0d
cgeek <cem.moreau@gmail.com>
2013-08-09 14:08:35 +0200

    Now displays dynamic info on Home page

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,32 @@ var async = require('async');
 module.exports = function (node, auth) {
   
   this.index = function(req, res){
-    res.render('index', {
-      currency: 'beta_brousoufs',
-      membersCount: 0,
-      amendmentsCount: 0,
-      transactionsCount: 0,
-      auth: auth
-    });
+    node.ucg.peering(proxy(res, function (json) {
+      res.render('index', {
+        currency: json.currency,
+        ipv4: json.ipv4,
+        ipv6: json.ipv6,
+        port: json.port,
+        dns: json.dns,
+        peers: json.peers,
+        fingerprint: json.key,
+        membersCount: 0,
+        amendmentsCount: 0,
+        transactionsCount: 0,
+        auth: auth
+      });
+    }));
   };
 
   return this;
+}
+
+function proxy(res, callback) {
+  return function (err, json) {
+    if(err){
+      res.send(500, err);
+      return;
+    }
+    else callback(json);
+  }
 }
