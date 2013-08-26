5838da14da4a317f7e720a2fb536b6f1a3e21d06
cgeek <cem.moreau@gmail.com>
2013-08-27 00:14:35 +0200

    Add Community > Registered

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var fs    = require('fs');
 var _     = require('underscore');
+var sha1  = require('sha1');
 
 module.exports = function (node, auth) {
   
@@ -10,27 +11,33 @@ module.exports = function (node, auth) {
       function (next){
         node.hdc.amendments.current(function (err, am) {
           if(am){
-            node.hdc.amendments.view.memberships(am.number, am.hash, function (err, mems) {
-              next(err, mems || []);
+            node.hdc.amendments.view.memberships(am.number, sha1(am.raw).toUpperCase(), { extract: true }, function (err, mems) {
+              next(err, mems.merkle.leaves || {});
             });
           }
-          else next(null, []);
+          else next(null, { merkle: {} });
         });
       },
       function (mems, next){
-        async.forEach(mems, function(item, callback){
-          status[item.fingerprint] = { membership: item };
-          node.pks.lookup('0x' + item.fingerprint, function (err, keys) {
-            if(keys.length == 0){
-              callback('Member\'s public key with FPR ' + '0x' + item.fingerprint + ' not stored!');
+        var indexes = [];
+        _(mems).each(function (item, index) {
+          indexes.push(index);
+        });
+        async.forEach(indexes, function(index, callback){
+          var item = mems[index].value;
+          status[item.issuer] = { membership: item.request };
+          node.pks.lookup('0x' + item.issuer, function (err, json) {
+            if(json.keys.length == 0){
+              callback('Member\'s public key with FPR ' + '0x' + item.issuer + ' not stored!');
               return;
             }
-            status[item.fingerprint].key = keys[0];
+            status[item.issuer].key = json.keys[0];
             callback(err);
           });
         }, next);
       }
     ], function (err) {
+      console.log(status);
       res.render('community/list', {
         status: _(status).values(),
         auth: auth
